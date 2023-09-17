0553366471f9324162e651bcc931fbb0f77b0e2f
Mickael <mickael.mesnage@lonestone.io>
2023-09-17 10:27:10 +0200

    :sparkles: add jwt authentication in backend

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { GraphqlModule } from './modules/graphql/graphql.module';
 import { MessageModule } from './modules/messages/message.module';
 import { OrmModule } from './modules/orm/orm.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [OrmModule, GraphqlModule, MessageModule],
+  imports: [OrmModule, GraphqlModule, MessageModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
